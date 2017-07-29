a25ad7867df9d90f4e76379417f482a59dd2cb75
Seung <skyhs3507@likelion.org>
2017-07-29 20:48:14 +0900

    ASDF

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  rolify
+  include Authority::UserAbilities
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
+  has_many :posts, dependent: :destroy
+end
